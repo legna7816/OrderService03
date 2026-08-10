803a623b6e6715150721f3932a3150d7cc39bf1a
legna7816 <tkdqlsfl02@gmail.com>
2026-08-10 14:43:51 +0900

    test

--- src/main/java/pack/OrderService03Application.java
+++ src/main/java/pack/OrderService03Application.java
@@ -11,7 +11,7 @@ public class OrderService03Application {
 	@GetMapping("/")
 	public String home() {
 		System.out.println("Hi");
-		return "14:25";
+		return "14:43";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(OrderService03Application.class, args);
